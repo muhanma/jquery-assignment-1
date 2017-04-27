4ed8aa0a97b51e5c1b0305b36409cca0662aef6c
muhanma <muhan.ma@id.iit.edu>
2017-04-27 16:06:45 -0500

    Update script.js

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,96 @@
 // Do your work in this file.
+// Do your work in this file.
+//1. Fork this repo.
+
+//2. Only code inside the file named script.js. No editing any other files.
+
+//3. Create a div element with a class name of "view" and append it to the document body using jQuery. You can use append, appendTo, or any other jQuery method.
+
+var view = document.createElement("div");
+view.className = "view";
+$( view ).appendTo(document.body);
+
+//4. Create a div element with a class name of "grass" and append it to the previously created "view" div from step 3.
+
+var grass = document.createElement("div");
+grass.className = "grass";
+$( grass ).appendTo( $(view) );
+
+//5.  Create a div element with a class name of "sun" and append it to the previously created "view" div from step 3.
+
+var sun = document.createElement("div");
+sun.className = "sun";
+$(sun).appendTo($(view));
+//6.  Write a while loop to create 5 divs. The div classes will be "ray-1", "ray-2", "ray-3", "ray-4", and "ray-5". Append each of these divs to the previously created "sun" div from step 5.
+
+var rayN = 1;
+while (rayN <= 5) {
+    var ray = document.createElement("div");
+    ray.className = "ray-"+rayN;
+   $(ray).appendTo($(sun));
+    rayN++;
+}
+
+//7. Create a div element with a class name of "mountain" and append it to the previously created "view" div from step 3.
+
+var mountain = document.createElement("div");
+mountain.className = "mountain";
+$(mountain).appendTo($(view));
+
+//8. Create a div element with a class name of "mountain-top" and append it to the previously created "mountain" div from step 7.
+
+var Top = document.createElement("div");
+Top.className = "mountain-top";
+$(Top).appendTo($(mountain));
+
+//9. Write a do...while loop to create 3 divs. The div classes will be "mountain-cap-1", "mountain-cap-2", and "mountain-cap-3". Append each of these divs to the previously created "mountain-top" div from step 
+var cap = 0;
+do {
+    var mountainCap = document.createElement("div");
+    mountainCap.className = 'mountain-cap-' + (cap + 1);    
+     $(mountainCap).appendTo($(Top));
+    cap ++;
+} while (cap <= 3);
+
+
+
+// My Previous Work
+
+// var view = document.createElement("div");
+// view.className = "view";
+// document.body.appendChild(view);
+
+
+// var grass = document.createElement("div");
+// grass.className = "grass";
+// view.appendChild(grass);
+
+
+// var sun = document.createElement("div");
+// sun.className = "sun";
+// view.appendChild(sun);
+
+// //--6. The div classes will be "ray-1", "ray-2", "ray-3", "ray-4", and "ray-5". 
+// var rayN = 1;
+// while (rayN <= 5) {
+//     var ray = document.createElement("div");
+//     ray.className = "ray-"+rayN;
+//     sun.appendChild(ray);
+//     rayN++;
+// }
+
+// var mountain = document.createElement("div");
+// mountain.className = "mountain";
+// view.appendChild(mountain);
+
+// var Top = document.createElement("div");
+// Top.className = 'mountain-top';    
+// mountain.appendChild(Top);
+
+// var cap = 0;
+// do {
+//     var mountainCap = document.createElement("div");
+//     mountainCap.className = 'mountain-cap-' + (cap + 1);    
+//     Top.appendChild(mountainCap);
+//     cap ++;
+// } while (cap <= 3);
